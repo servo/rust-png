298ef5d620bdb09dea353de9d5babcf433b93892
Ryan Choi <ryan.choi@samsung.com>
2013-11-19 16:08:53 +0900

    add support for transparent png

--- ffi.rs
+++ ffi.rs
@@ -19,10 +19,14 @@ pub static COMPRESSION_TYPE_DEFAULT: c_int = 0;
 
 pub static COLOR_TYPE_GRAY: c_int = 0;
 pub static COLOR_TYPE_RGB: c_int = 2;
+pub static COLOR_TYPE_PALETTE: c_int = 3;
 pub static COLOR_TYPE_GRAY_ALPHA: c_int = 4;
 pub static COLOR_TYPE_GA: c_int = 4;
 pub static COLOR_TYPE_RGB_ALPHA: c_int = 6;
 pub static COLOR_TYPE_RGBA: c_int = 6;
+
+pub static FILLER_AFTER: c_int = 1;
+pub static INFO_tRNS: c_int = 0x0010;
 
 pub type png_struct = c_void;
 pub type png_info = c_void;
@@ -46,8 +50,8 @@ extern {
     pub fn png_destroy_read_struct(png_ptr_ptr: **png_struct, info_ptr_ptr: **png_info, end_info_ptr_ptr: **png_info);
     pub fn png_set_read_fn(png_ptr: *mut png_struct, io_ptr: *mut c_void, read_data_fn: extern "C" fn(*png_struct, *mut u8, size_t));
     pub fn png_read_info(png_ptr: *mut png_struct, info_ptr: *mut png_info);
+    pub fn png_read_update_info(png_ptr: *mut png_struct, info_ptr: *mut png_info);
     pub fn png_read_image(png_ptr: *mut png_struct, row_pointers: **mut u8);
-    pub fn png_read_png(png_ptr: *mut png_struct, info_ptr: *mut png_info, transforms: c_int, params: *c_void);
 
     pub fn png_create_write_struct(user_png_ver: *c_char, error_ptr: *c_void, error_fn: *u8, warn_fn: *u8) -> *mut png_struct;
     pub fn png_destroy_write_struct(png_ptr_ptr: **png_struct, info_ptr_ptr: **png_info);
@@ -65,4 +69,11 @@ extern {
     pub fn png_set_IHDR(png_ptr: *png_struct, info_ptr: *mut png_info, width: u32, height: u32, bit_depth: c_int, color_type: c_int, interlace_method: c_int, compression_method: c_int, filter_method: c_int);
     pub fn png_set_pHYs(png_ptr: *png_struct, info_ptr: *mut png_info, res_x: u32, res_y: u32, unit_type: c_int);
     pub fn png_set_rows(png_ptr: *png_struct, info_ptr: *mut png_info, row_pointers: **u8);
+
+    pub fn png_set_packing(png_ptr: *mut png_struct);
+    pub fn png_set_palette_to_rgb(png_ptr: *mut png_struct);
+    pub fn png_set_expand_gray_1_2_4_to_8(png_ptr: *mut png_struct);
+    pub fn png_set_tRNS_to_alpha(png_ptr: *mut png_struct);
+    pub fn png_set_filler(png_ptr: *mut png_struct, val: u32, flag: c_int);
+    pub fn png_set_interlace_handling(png_ptr: *mut png_struct);
 }
