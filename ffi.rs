4c27ef2961bb6df512df093dbf800a6bb26fdccb
Josh Matthews <josh@joshmatthews.net>
2014-03-14 15:12:29 -0400

    Warning police.

--- ffi.rs
+++ ffi.rs
@@ -7,6 +7,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#[allow(non_camel_case_types)];
+
 use std::libc::{c_int, size_t, c_void, c_char};
 
 pub static TRANSFORM_IDENTITY: c_int = 0;
